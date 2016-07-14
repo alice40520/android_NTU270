7dfe29da65aa12cd025b3cc141fb2eafd88c7bdc
YI-HSUAN WU <yihsuanw@sfu.ca>
2016-07-14 10:10:51 +0800

    Add DrinkMenuActivity logs

--- SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
@@ -2,6 +2,7 @@ package com.example.user.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class DrinkMenuActivity extends AppCompatActivity {
 
@@ -9,5 +10,43 @@ public class DrinkMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_menu);
+        Log.d("Debug", "DrinkMenunActivity onCreate");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Debug", "DrinkMenuActivity OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Debug", "DrinkMenuActivity OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Debug", "DrinkMenuActivity OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Debug", "DrinkMenuActivity OnStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Debug", "DrinkMenuActivity OnDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Debug", "DrinkMenuActivity OnRestart");
+    }
+
 }
